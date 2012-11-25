203b6a865464e476d0f254756af7f8e61ce543e8
vidirr <vidirr@gmail.com>
2012-11-25 17:19:09 +0000

    Merged test.py into tests.py (typographical error)

--- tictascii/ticlib/tests.py
+++ tictascii/ticlib/tests.py
@@ -1,9 +1,12 @@
 import unittest
+import random
 
 from tictascii.ticlib.base import Board
+from tictascii.ticlib.players import Player, HumanPlayer
 
 
 class BoardTest(unittest.TestCase):
+
     def setUp(self):
         self.board = Board()
 
@@ -13,3 +16,33 @@ class BoardTest(unittest.TestCase):
             [None, None, None],
             [None, None, None],
         ])
+
+
+class PlayerTest(unittest.TestCase):
+
+    def setUp(self):
+        self.player = Player()
+
+    def testInitialPlayer(self):
+        self.assertEquals(self.player.get_wins(), 0)
+
+    def testIncrementWins(self):
+        self.player = Player()
+        self.player.increment_wins()
+        self.assertEquals(self.player.get_wins(), 1)
+
+    def testGamesWon(self):
+        self.player = Player()
+        self.assertEquals(self.player.get_wins(), 0)
+
+
+def raw_input_mock():
+    return random.randint(1, 9)
+
+
+class HumanPlayerTest(unittest.TestCase):
+
+    raw_input = raw_input_mock
+
+    def setUp(self):
+        self.player = HumanPlayer()
