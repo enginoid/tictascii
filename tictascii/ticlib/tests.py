c8f22bd7c3ae4d3d8b3f39628e3a2d3567fde5d2
Fred Jonsson <fridrik@pyth.net>
2012-11-25 16:59:16 +0000

    Add `Board._get_winning_marker` to check winner.

--- tictascii/ticlib/tests.py
+++ tictascii/ticlib/tests.py
@@ -13,3 +13,38 @@ class BoardTest(unittest.TestCase):
             [None, None, None],
             [None, None, None],
         ])
+
+    def testHorizontalWinningMarker(self):
+        self.assertEquals('X', self.board._get_winning_marker([
+            ['X', 'X', 'X'],
+            [None, None, None],
+            [None, None, None],
+        ]))
+
+    def testVerticalWinningMarker(self):
+        self.assertEquals('O', self.board._get_winning_marker([
+            ['O', None, None],
+            ['O', None, None],
+            ['O', None, None],
+        ]))
+
+    def testDownRightDiagonalWinningMarker(self):
+        self.assertEquals('O', self.board._get_winning_marker([
+            ['O', None, None],
+            [None, 'O', None],
+            [None, None, 'O'],
+        ]))
+
+    def testDownLeftDiagonalWinningMarker(self):
+        self.assertEquals('X', self.board._get_winning_marker([
+            [None, None, 'X'],
+            [None, 'X', None],
+            ['X', None, None],
+        ]))
+
+    def testNoWinningMarker(self):
+        self.assertEquals(None, self.board._get_winning_marker([
+            ['O', 'O', 'X'],
+            ['X', None, 'X'],
+            ['X', 'X', 'O'],
+        ]))
